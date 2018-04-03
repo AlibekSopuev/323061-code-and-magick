307e40ff852667e25269a2ab85174a43fbd7eaca
Alibek <alibek92@mail.ru>
2018-04-03 21:40:31 +0600

    Канвас

---
+++ js/stat.js
@@ -0,0 +1,45 @@
+'use strict';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 150;
+var barWidth = 40;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+ window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText ('Ура вы победили!', 120, 30);
+  ctx.fillText ('Список результатов:', 120, 50);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], CLOUD_X + GAP + 30 * i, CLOUD_Y + GAP + 30 + FONT_GAP + (GAP + BAR_HEIGHT));
+    ctx.fillRect(CLOUD_X + GAP + 20 + TEXT_WIDTH * i, CLOUD_Y + GAP + 50, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+  }//заново рассчитать координаты колонок и их размеры с учетом констант Barheight и barwidth, расположение исправить, сейчас каждая новая колонка получает место в зависимости от высоты
+};
